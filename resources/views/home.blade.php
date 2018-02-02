1be24b741b445ea8e845381c36fbb1c94c4e4c9f
Noodles <roy@roys-MacBook-Pro.local>
2018-02-02 13:57:40 -0800

    Replaced Home with includes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,127 +1,38 @@
+<?php
+include '/home/mywallah/public_html/100words.net/includes/initialize.php';
+?>
+<?php $signedIn = "no" ?>
+<?php $pageTitle = "Home" ?>
+<?php $pageNav = "Home" ?>
+
+
+<?php
+if ($session->is_logged_in() == TRUE) {
+    $signedIn = "yes";
+    // echo 'logged in';
+}
+?>
+
+<?php $startTimeStamp = strtotime("2001/05/01");
+$today = date("Y/m/d");
+$endTimeStamp = strtotime($today);
+$timeDiff = abs($endTimeStamp - $startTimeStamp);
+$numberDays = $timeDiff/86400;
+$numberDays = intval($numberDays);
+$numberDays = number_format($numberDays);
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="art/favicon.png">
-
-        <title>100 Words -- Home Page</title>
-
-        <!-- Bootstrap core CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-        <!-- Custom styles for this template -->
-        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/additions.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/top-nav.css" rel="stylesheet" type="text/css" />
-        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-    
-        <link rel="stylesheet" type="text/css" href="css/buttons.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-        <script src="../../assets/js/html5shiv.js"></script>
-        <script src="../../assets/js/respond.min.js"></script>
-        <![endif]-->
-   </head>
+<?php include 'head.php';?>
 
 <body>
-        <!-- Navigation Bar-->
-        <header id="topnav">
-            <div class="topbar-main">
-                <div class="container">
+<?php include 'nav-main.php';?>
 
-                    <!-- Logo container-->
-                    <div class="logo hidden-xs">
-                        <a href="index.php" class="logo"><span><img src="art/logo-header.png"></span> </a>
-                    </div>
-                    <div class="logo hidden-sm hidden-md hidden-lg visible-xs-*">
-                        <a href="index.php" class="logo"><span><img src="art/logo-header-xs.png"></span> </a>
-                    </div>
-                    <!-- End Logo container-->
-
-                    <div class="menu-extras">
-
-                        <ul class="nav navbar-nav navbar-right pull-right">
-                            <li>
-                                <form role="search" class="navbar-left app-search pull-left hidden-xs">
-                                     <input type="text" placeholder="Search..." class="form-control">
-                                     <a href="search.php"><i class="fa fa-search"></i></a>
-                                </form>
-                            </li>
-
-                            <li class="dropdown">
-                                    <li><a href="user-login.php">Sign In</a></li>
-                            </li>
-
-                        </ul>
-
-                        <div class="menu-item">
-                            <!-- Mobile menu toggle-->
-                            <a class="navbar-toggle">
-                                <div class="lines">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </a>
-                            <!-- End mobile menu toggle-->
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <!-- End topbar -->
-
-
-            <!-- Navbar Start -->
-            <div class="navbar-custom">
-                <div class="container">
-                <div id="navigation">
-                    <!-- Navigation Menu-->
-                    <ul class="navigation-menu">
-                        <li class="has-submenu ">
-                            <a href="read-by-batch.php"><i class="fa fa-eye"></i>Read</a>
-<!--                            <ul class="submenu">
-                                <li><a href="read-by-batch.php">By Batch</a></li>
-                                <li><a href="read-by-date.php">By Date</a></li>
-                                <li><a href="read-by-writer.php">By Writer</a></li>
-                                <li><a href="read-random.php">Random Entry</a></li>
-                            </ul>
--->
-                        </li>
-                        <li class="has-submenu ">
-                            <a href="write-choose-batch.php"><i class="fa fa-pencil"></i>Write</a>
-                        </li>
-                        <li class="has-submenu ">
-                            <a href="members-overview.php"><i class="fa fa-user"></i>Meet Our Writers</a>
-                        </li>
-                        <li class="has-submenu ">
-                            <a href="batch-create.php"><i class="fa fa-gears"></i>Create a Batch</a>
-                        </li>
-                        <li class="has-submenu hidden-sm hidden-md hidden-lg visible-xs-* ">
-                            <a href="search.php"><i class="fa fa-search"></i>Search</a>
-                        </li>
-
-                        <li class="has-submenu ">
-                            <a href="user-register.php"><i class="fa fa-thumbs-up"></i>Join Us</a>
-                        </li>
-
-
-
-                    </ul>
-                    <!-- End navigation menu -->
-                </div>
-            </div>
-            </div>
-        </header>
-        <!-- End Navigation Bar-->
 <div class="container">
 <div class="col-md-12 text-center">
-<h1 class="text-center" style="padding-top:100px;">1,444 writers<br>6,059 days<br>16,676,300 published words</h1>
+<h1 class="text-center" style="padding-top:100px;">1,389 writers<br><?php echo $numberDays; ?> days<br>16,676,300 published words</h1>
 </div>
 </div>
 
@@ -149,41 +60,9 @@
 </div>
 
 
-<div class="footer">
-    <div class="container">
+<?php include 'footer.php';?>
 
-        <div class="row">
-
-            <div class="col-sm-4 col-xs-12">
-                <p><strong>&copy; 2017 <a href="http://chaoticneutral.org/" target="_blank">Chaotic Neutral LLC</a></strong></p>
-                <p>All rights reserved</p>
-            </div>      
-
-            <div class="col-sm-8 col-xs-12">
-                <p class="footer-links">
-                    <a href="roy-read-single-entry-base.php">Special Link for Roy Second</a>
-                    <a href="admin-about.php">About</a>
-                    <a href="admin-terms.php">Terms & Conditions</a>
-                    <a href="admin-contact.php">Contact Us</a>
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
-<!-- <script src="bootstrap/js/bootstrap.js"></script>
-<script src="js/jquery.flot.js"></script>
-<script src="js/global.js"></script>
-<script src="js/jquery.app.js"></script>
-<script src="js/dropzone.js"></script> -->
+<?php include 'javascript.php';?>
 
 </body>
 </html>
