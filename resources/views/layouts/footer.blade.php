563000abc1c55defb085175d7b449aac02c6b60f
Noodles <roy@roys-MacBook-Pro.local>
2018-02-05 08:51:10 -0800

    More work on layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="bootstrap/js/bootstrap.js"></script>
+<script src="js/jquery.flot.js"></script>
+<script src="js/global.js"></script>
+<script src="js/jquery.app.js"></script>
+<script src="js/dropzone.js"></script>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="art/favicon.png">
+
+        <title>100 Words -- Home</title>
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" rel="stylesheet">
+ 
+        <!-- Custom styles for this template -->
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/additions.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/top-nav.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="css/buttons.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="../../assets/js/html5shiv.js"></script>
+        <script src="../../assets/js/respond.min.js"></script>
+        <![endif]-->
+   </head>
+
